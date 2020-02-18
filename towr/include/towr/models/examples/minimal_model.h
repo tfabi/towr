326c9ce7aad47ba550e7aa092c0ae7d293007fb1
Fabian Tischhauser <tfabian@student.ethz.ch>
2020-02-18 10:19:27 +0100

    added minimal robot

--- towr/include/towr/models/examples/minimal_model.h
+++ towr/include/towr/models/examples/minimal_model.h
@@ -27,47 +27,46 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ******************************************************************************/
 
-#include <towr/models/robot_model.h>
+#ifndef TOWR_TOWR_ROS_INCLUDE_TOWR_ROS_MINIMAL_MODEL_H_
+#define TOWR_TOWR_ROS_INCLUDE_TOWR_ROS_MINIMAL_MODEL_H_
 
-#include <towr/models/examples/monoped_model.h>
-#include <towr/models/examples/biped_model.h>
-#include <towr/models/examples/hyq_model.h>
-#include <towr/models/examples/anymal_model.h>
-#include <towr/models/examples/minimal_model.h>
+#include <towr/models/kinematic_model.h>
+#include <towr/models/single_rigid_body_dynamics.h>
+#include <towr/models/endeffector_mappings.h>
 
 namespace towr {
 
+/**
+ * @brief The Kinematics of the quadruped class mini.
+ */
+class MinimalKinematicModel : public KinematicModel {
+public:
+  MinimalKinematicModel () : KinematicModel(4)
+  {
+    const double x_nominal_b = 0.18;
+    const double y_nominal_b = 0.075;
+    const double z_nominal_b = -0.2;
 
-RobotModel::RobotModel(Robot robot)
-{
-  switch (robot) {
-    case Monoped:
-      dynamic_model_   = std::make_shared<MonopedDynamicModel>();
-      kinematic_model_ = std::make_shared<MonopedKinematicModel>();
-      break;
-    case Biped:
-      dynamic_model_   = std::make_shared<BipedDynamicModel>();
-      kinematic_model_ = std::make_shared<BipedKinematicModel>();
-      break;
-    case Hyq:
-      dynamic_model_   = std::make_shared<HyqDynamicModel>();
-      kinematic_model_ = std::make_shared<HyqKinematicModel>();
-      break;
-    case Anymal:
-      dynamic_model_   = std::make_shared<AnymalDynamicModel>();
-      kinematic_model_ = std::make_shared<AnymalKinematicModel>();
-      break;
-    case Minimal:
-      dynamic_model_   = std::make_shared<MinimalDynamicModel>();
-      kinematic_model_ = std::make_shared<MinimalKinematicModel>();
-      break;
-    default:
-      assert(false); // Error: Robot model not implemented.
-      break;
+    nominal_stance_.at(LF) <<  x_nominal_b,   y_nominal_b, z_nominal_b;
+    nominal_stance_.at(RF) <<  x_nominal_b,  -y_nominal_b, z_nominal_b;
+    nominal_stance_.at(LH) << -x_nominal_b,   y_nominal_b, z_nominal_b;
+    nominal_stance_.at(RH) << -x_nominal_b,  -y_nominal_b, z_nominal_b;
+
+    max_dev_from_nominal_ << 0.075, 0.075, 0.1;
   }
-}
+};
 
+/**
+ * @brief The Dynamics of the quadruped class MiniMal.
+ */
+class MinimalDynamicModel : public SingleRigidBodyDynamics {
+public:
+  MinimalDynamicModel()
+  : SingleRigidBodyDynamics(3.0,
+                    0.018442, 0.043582, 0.049515, 0, 0, 0,
+                    4) {}
+};
 
 } // namespace towr
 
-
+#endif /* TOWR_TOWR_ROS_INCLUDE_TOWR_ROS_MINIMAL_MODEL_H_ */
